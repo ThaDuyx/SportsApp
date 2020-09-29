96949716391b7285c8379da4f528def06cc4315f
ChrisMizz <Christofferdetlef@gmail.com>
2020-09-29 12:14:55 +0200

    - Change which storyboard to open first - Added more folders for the different views that have to be made - Added logo and background for login

---
+++ Sportster/Intro/IntroViewContrtoller.swift
@@ -0,0 +1,8 @@
+//
+//  IntroViewContrtoller.swift
+//  Sportster
+//
+//  Created by Christoffer Detlef on 29/09/2020.
+//
+
+import Foundation
---
+++ Sportster/Login/LoginViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  Sportster
+//
+//  Created by Christoffer Detlef on 29/09/2020.
+//
+
+import Foundation
