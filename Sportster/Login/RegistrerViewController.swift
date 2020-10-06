142923fa80816343b33eb3fd9bfc6e3ec13ae100
ChrisMizz <Christofferdetlef@gmail.com>
2020-10-06 10:46:21 +0200

    - Email login design made - Fix so textfield moves up when keyboard shows (Needed) - Logo for facebook on login added and changed

--- Sportster/Login/RegistrerViewController.swift
+++ Sportster/Login/RegistrerViewController.swift
@@ -28,9 +28,12 @@ extension UIColor {
 
 class RegistrerViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailLoginLabel: UILabel!
     @IBOutlet weak var registerButton: UIButton!
     @IBAction func registerButtonTapped(_ sender: Any) {
-
+        
         let storyboard = UIStoryboard(name: "Intro", bundle: nil)
         let vc = storyboard.instantiateViewController(identifier: "IntroVC")
         
@@ -43,11 +46,35 @@ class RegistrerViewController: UIViewController {
         super.viewDidLoad()
         self.navigationController?.navigationBar.barTintColor = UIColor.init(rgb: 0x2AC0C0)
         registerButton.layer.cornerRadius = 20
+        
+        //https://stackoverflow.com/a/59463814
+        //----------------------------------------
+            let titleText = "Email Login_"
+            var charIndex = 0.0
+            for letter in titleText {
+                Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                    self.emailLoginLabel.text?.append(letter)
+                }
+                 charIndex += 1
+            }
+        //----------------------------------------
+        
+        //-- Border for Textfield START
+        let borderColor = UIColor.init(rgb: 0x2AC0C0)
+        emailTextField.layer.borderColor = borderColor.cgColor
+        passwordTextField.layer.borderColor = borderColor.cgColor
+
+        emailTextField.layer.borderWidth = 1.0
+        passwordTextField.layer.borderWidth = 1.0
+        
+        emailTextField.layer.cornerRadius = 20
+        passwordTextField.layer.cornerRadius = 20
+        //-- Border for Textfield END
+
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        self.navigationController?.setNavigationBarHidden(true, animated: true)
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
     }
 
 }
-
