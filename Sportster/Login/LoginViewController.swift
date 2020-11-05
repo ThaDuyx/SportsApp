e2a93e4c98ae7afbc9f92344e356e2767e65ad42
Simon Andersen <sandersen98@live.dk>
2020-11-05 15:24:09 +0100

    Added login to Facebook and Google now - No segues are added yet - Next step is to integrate Google and Facebook login with user information

--- Sportster/Login/LoginViewController.swift
+++ Sportster/Login/LoginViewController.swift
@@ -7,23 +7,79 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import FBSDKCoreKit
+import FBSDKLoginKit
+import FBSDKShareKit
+import GoogleSignIn
 
-class LoginViewController: UIViewController {
 
+class LoginViewController: UIViewController, GIDSignInDelegate {
+    
+    
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+            
+        }
+        guard let auth = user.authentication else { return }
+        let credentials = GoogleAuthProvider.credential(withIDToken: auth.idToken, accessToken: auth.accessToken)
+        Auth.auth().signIn(with: credentials) { (authResult, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                
+            } else {
+                print("Login Successful.")
+                //This is where you should add the functionality of successful login
+                //i.e. dismissing this view or push the home view controller etc
+                
+            }
+            
+        }
+}
+    
+
+    @IBAction func fButtonTapped(_ sender: Any) {
+        let loginManager = LoginManager()
+        loginManager.logIn(permissions: [.publicProfile, .email], viewController: self) { (result) in
+            
+            let credential = FacebookAuthProvider.credential(withAccessToken: AccessToken.current!.tokenString)
+            Auth.auth().signIn(with: credential) { (user, error) in
+                if let error = error {
+                    print("Login error: \(error.localizedDescription)")
+                    let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
+                    let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(okayAction)
+                    self.present(alertController, animated: true, completion: nil)
+                    return
+                }
+            }
+        }
+    }
+    
+    @IBAction func gButtonTapped(_ sender: Any) {
+        GIDSignIn.sharedInstance().signIn()
+    }
+    
+    
     @IBOutlet weak var topBoxView: UIView!
-    @IBOutlet weak var fLoginButton: UIButton!
     @IBOutlet weak var gLoginButton: UIButton!
+    @IBOutlet weak var fLoginButton: UIButton!
     @IBOutlet weak var eLoginButton: UIButton!
     override func viewDidLoad() {
         self.navigationController?.setNavigationBarHidden(true, animated: true)
         super.viewDidLoad()
-        
         topBoxView.layer.cornerRadius = topBoxView.frame.size.width/2
         topBoxView.clipsToBounds = true
-        
-        fLoginButton.layer.cornerRadius = 20
         gLoginButton.layer.cornerRadius = 20
         eLoginButton.layer.cornerRadius = 20
+        fLoginButton.layer.cornerRadius = 20
+        
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance().delegate = self
+        
     }
-    
+
 }
