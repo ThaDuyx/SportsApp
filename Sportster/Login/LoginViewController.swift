89568d2747cb6a84d290901d1ec8564392119297
Simon Andersen <sandersen98@live.dk>
2021-01-07 14:06:06 +0100

    New added functionality to FB login - though commented cause we cannot really use it yet.

--- Sportster/Login/LoginViewController.swift
+++ Sportster/Login/LoginViewController.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import UIKit
-import FirebaseAuth
+import Firebase
 import FBSDKCoreKit
 import FBSDKLoginKit
 import FBSDKShareKit
@@ -49,22 +49,26 @@ class LoginViewController: UIViewController, GIDSignInDelegate {
                 return
             }
                 
-                let credential = FacebookAuthProvider.credential(withAccessToken: AccessToken.current!.tokenString)
-                Auth.auth().signIn(with: credential) { (user, error) in
-                    if let error = error {
-                        print("Login error: \(error.localizedDescription)")
-                        let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
-                        let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                        alertController.addAction(okayAction)
-                        self.present(alertController, animated: true, completion: nil)
-                    } else {
-                        print("Login successfull")
-                        let storyboard = UIStoryboard(name: "Intro", bundle: nil)
-                        let vc = storyboard.instantiateViewController(identifier: "IntroVC")
-                        self.view.window?.rootViewController = vc
-                        self.view.window?.makeKeyAndVisible()
-                    }
-            }
+            let credential = FacebookAuthProvider.credential(withAccessToken: AccessToken.current!.tokenString)
+            Auth.auth().signIn(with: credential) { (user, error) in
+                
+                if let error = error {
+                    print("Login error: \(error.localizedDescription)")
+                    let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
+                    let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(okayAction)
+                    self.present(alertController, animated: true, completion: nil)
+                } else {
+                    print("Login successfull")
+                    //let newFBUserRef = Firestore.firestore().collection("user").document((user?.user.uid)!)
+                    //newFBUserRef.setData(["uid" : user?.user.uid as Any])
+                    self.getFBUserData()
+                    let storyboard = UIStoryboard(name: "Intro", bundle: nil)
+                    let vc = storyboard.instantiateViewController(identifier: "IntroVC")
+                    self.view.window?.rootViewController = vc
+                    self.view.window?.makeKeyAndVisible()
+                }
+        }
     }
 }
     
@@ -94,5 +98,34 @@ class LoginViewController: UIViewController, GIDSignInDelegate {
         self.navigationController?.setNavigationBarHidden(true, animated: true)
         
     }
-    
+    //https://stackoverflow.com/a/50447480
+    //___________________________________________
+    func getFBUserData(){
+        GraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, email"]).start(completionHandler:
+                { (connection, result, error) -> Void in
+                    if (error == nil)
+                    {
+                        //everything works print the user data
+                        //                        print(result!)
+                        if let data = result as? NSDictionary
+                        {
+                            let firstName  = data.object(forKey: "first_name") as? String
+                            let lastName  = data.object(forKey: "last_name") as? String
+
+
+                            if let email = data.object(forKey: "email") as? String
+                            {
+                                print(email)
+   
+                            }
+                            else
+                            {
+                                 // If user have signup with mobile number you are not able to get their email address
+                                print("We are unable to access Facebook account details, please use other sign in methods.")
+                            }
+                        }
+                    }
+            })
+    }
+    //___________________________________________
 }
