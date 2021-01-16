284fa4e73303d8b5a5dac765bf83517145c83367
ChrisMizz <Christofferdetlef@gmail.com>
2021-01-16 15:17:04 +0100

    Made two UItest for when you are logging in

--- SportsterUITests/SportsterUITests.swift
+++ SportsterUITests/SportsterUITests.swift
@@ -17,26 +17,89 @@ class SportsterUITests: XCTestCase {
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func testLoginErrors() {
+        let invalidEmail = "TestTest.com"
+        let invalidPass = "testtestt"
+        let validEmail = "test@test.com"
+        let validPass = "testtest"
+        var loginTest = 0
+        
         let app = XCUIApplication()
         app.launch()
+        
+        while loginTest <= 1 {
+            if loginTest == 0 {
+                let app = XCUIApplication()
+                app.staticTexts["Forsæt med E-mail"].tap()
+                let email = app.textFields["Email"]
+                let pass = app.secureTextFields["Kodeord"]
+                
+                //Wrong email but correct password
+                email.tap()
+                email.typeText(invalidEmail)
+                pass.tap()
+                pass.typeText(validPass)
+                app.buttons["Log ind"].tap()
+                
+                //Correct email but wrong password
+                email.tap()
+                email.buttons["Clear text"].tap()
+                email.typeText(validEmail)
+                pass.tap()
+                pass.buttons["Clear text"].tap()
+                pass.typeText(invalidPass)
+                app.buttons["Log ind"].tap()
+                
+                loginTest += 1
+            } else {
+                let email = app.textFields["Email"]
+                let pass = app.secureTextFields["Kodeord"]
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
+                email.tap()
+                email.buttons["Clear text"].tap()
+                email.typeText(validEmail)
+                pass.tap()
+                pass.buttons["Clear text"].tap()
+                pass.typeText(validPass)
+                app.buttons["Log ind"].tap()
+                
+                loginTest += 1
             }
         }
+    }
+    
+    func testValidLoginSuccess(){
+        let validEmail = "test@test.com"
+        let validPass = "testtest"
+        var loginTest = 0
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        while loginTest <= 1 {
+            if loginTest == 0 {
+                let app = XCUIApplication()
+                app.staticTexts["Forsæt med E-mail"].tap()
+                app.staticTexts["Log ind"].tap()
+                app.alerts["Hov, du har vist glemt noget"].scrollViews.otherElements.buttons["Prøv igen"].tap()
+                loginTest += 1
+            } else {
+                let userName = app.textFields["Email"]
+                userName.tap()
+                userName.typeText(validEmail)
+                
+                let passwordSecureTextField = app.secureTextFields["Kodeord"]
+                passwordSecureTextField.tap()
+                passwordSecureTextField.typeText(validPass)
+                
+                app.buttons["Log ind"].tap()
+                loginTest += 1
+            }
+        }
+    }
+    
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 }
